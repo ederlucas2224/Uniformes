11fddea29a569f2e1ae364d59d0abfd9cb3019bd
ederlucas2224 <77474392+ederlucas2224@users.noreply.github.com>
2023-09-11 12:40:24 -0600

    Add files via upload

---
+++ BD/InsertsBD.sql
@@ -0,0 +1,11 @@
+insert into Tipo values('Sindicalizados'),('Confianza')
+
+insert into Grupos values('Mantenimiento Movil',1),('Cabulleros Buque',1),('Tractos',1),('Patio',1),('Almacen',1),('Administrativos',2)
+
+insert into Empleados values('Alexis','Javier','Corcuera','Solis',1,6),('Eladio','','Hinojosa','Hernandez',1,1),('Saul','','Carpacio','Trinidad',1,1),
+							('Arnoldo','','Sanchez','Luna',1,2),('Hector','Miguel','Gomez','Flores',1,3),('Denzel','Sebastian','Mojica','Godoy',1,4),
+							('Luis','Fernando','Gallegos','Arellano',1,6),('Edgar','Oswaldo','Lara','Gaviño',1,6),('Omar','Jonathan','Quiroz','Cardenas',1,1),
+							('Victor','Manuel','Aguilar','Vargas',1,1),('Juan','','Cienfuegos','Pimentel',1,3),('Ruben','','Jimenez','De La Rosa',1,6),
+							('Fidel','','Gonzales','Gonzales',1,1)
+
+insert into Producto values('Camisas',160,460,6),('Chamarras',100,500,6),('Pantalones',140,800,6),('cascos',300,90,1),('botas de casquillo',90,800,1)
